ce262a94ed4fee09f78271bbee0bc2f0d630a99b
Deanne Chae <126113364+skyoon26@users.noreply.github.com>
2025-01-27 11:05:40 -0600

    Set up Java backend file structure and configure MySQL connection

---
+++ personal-finance-tracker-api/src/main/java/com/example/personal_finance_tracker_api/PersonalFinanceTrackerApiApplication.java
@@ -0,0 +1,13 @@
+package com.example.personal_finance_tracker_api;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PersonalFinanceTrackerApiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(PersonalFinanceTrackerApiApplication.class, args);
+	}
+
+}
---
+++ personal-finance-tracker-api/src/test/java/com/example/personal_finance_tracker_api/PersonalFinanceTrackerApiApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.personal_finance_tracker_api;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PersonalFinanceTrackerApiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
